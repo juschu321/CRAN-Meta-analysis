e09781511176a241423038513ec308f7a4feee6d
mbannert <matthias.bannert@gmail.com>
2019-06-30 14:30:03 +0200

    started with minimal skeleton to build interface for app

---
+++ R/get_cranlogs.R
@@ -0,0 +1,9 @@
+#' Get Daily Download Statistics from CRAN logs
+#'
+#'
+#' @param pkgs character vector, names of packages in question.
+#' @return data.table containing the download statistics of multiple packages.
+#' @param export 
+get_cranlogs <- function(pkgs){
+
+}
---
+++ R/list_packages_in_ctv.R
@@ -0,0 +1,9 @@
+#' List all packages in a particular CRAN Task View
+#'
+#'
+#' @param ctv character name of the ctv
+#' @return character vector containing names of packages
+#' @export
+list_packages_in_ctv <- function(ctv){
+
+}
